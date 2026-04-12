d47bbfda70e0518172b7ff5dabb87315d5ca533f
Alejandra <alejandra.escobar@ucb.edu.bo>
2026-04-12 19:22:23 -0400

    test: should return error when zone has no schedule assigned

--- src/functions/EMSA/Schedule.test.js
+++ src/functions/EMSA/Schedule.test.js
@@ -7,6 +7,9 @@ describe("Schedule", () => {
     expect(result).toEqual("Tuesday and Friday, 07:00 - 09:00");
   });
 
-  
+  it("should return an error when the zone has no schedule assigned", () => {
+  const result = get_schedule("Zona Desconocida");
+  expect(result).toEqual("No schedule found for this zone");
+});
 
   });
