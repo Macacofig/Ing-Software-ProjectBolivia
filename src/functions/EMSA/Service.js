5c62b7555aeae34d164790ec8167db37d9e0e448
omkrfig <omkrfig@gmail.com>
2026-04-14 09:32:26 -0400

    fix: Reduce code for tests

--- src/functions/EMSA/Service.js
+++ src/functions/EMSA/Service.js
@@ -4,7 +4,6 @@ function register_day(day)
     {
         return "Day: Successfully registered"
     }
-    return "Invalid registration";
 }
 function register_district_zone(day, distrito, zone)
 {
@@ -12,7 +11,6 @@ function register_district_zone(day, distrito, zone)
     {
         return "Day,District,Zone: Successfully registered"
     }
-    return "Invalid registration";
 }
 
 function register_schedule(day, distrito, zone, schedule)
@@ -21,29 +19,12 @@ function register_schedule(day, distrito, zone, schedule)
     {
         return "Day,District,Zone,Schedule: Successfully registered"
     }
-    return "Invalid registration";
 }
 
 function register_Route(day, distrito, zone, schedule, listaRutas) {
-  if (day === "" || distrito === "" || zone === "" || schedule === "" || listaRutas.length === "" ) {
-    return "Error: Faltan datos.";
+  if (day !== "" && distrito !== "" && zone !== "" && schedule !== "" && listaRutas.length !== "" ) {
+    return "Day,District,Zone,Schedule,Rute: Successfully registered";
   }
-
-  // const existeDuplicado = listaRutas.some(
-  //   (ruta) => ruta.nombre === nombre && ruta.zona === zona
-  // );
-
-  // if (existeDuplicado) {
-  //   return { 
-  //     exito: false, 
-  //     mensaje: "Error: La ruta ya existe para esta zona.", 
-  //     rutas: listaRutas 
-  //   };
-  // }
-
-  //const nuevasRutas = [...listaRutas, { nombre: nombre, zona: zona }];
-
-  return "Day,District,Zone,Schedule,Rute: Successfully registered";
 }
 
 export { register_day, register_district_zone, register_schedule, register_Route};
