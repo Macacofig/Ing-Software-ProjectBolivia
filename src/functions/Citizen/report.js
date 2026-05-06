92013ab4f8832da1c2620746b992e59cf143b92d
omkrfig <omkrfig@gmail.com>
2026-05-05 23:56:10 -0400

    feat: control of location empty

--- src/functions/Citizen/report.js
+++ src/functions/Citizen/report.js
@@ -3,6 +3,9 @@ function register_report(idCitizen, description, location)
     if(description === "") {
         return "Description cannot be empty";
     }
+    if(location === "") {
+        return "Location cannot be empty";
+    }
     return "Report: Successfully registered";
 }
 
--- src/functions/Citizen/report.test.js
+++ src/functions/Citizen/report.test.js
@@ -8,4 +8,7 @@ describe("Reports", () => {
   it("It should show an error if description is empty", () => {
     expect(register_report(1,"", "caracoles")).toEqual("Description cannot be empty");
   });
+  it("It should show an error if location is empty", () => {
+    expect(register_report(1,"There is accumulated garbage", "")).toEqual("Location cannot be empty");
+  });
 });
