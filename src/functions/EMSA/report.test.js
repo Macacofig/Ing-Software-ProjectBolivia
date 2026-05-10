908a788795d88f3bb8d986e0c8727b00edf6145b
omkrfig <omkrfig@gmail.com>
2026-05-10 11:52:29 -0400

    feat: Show filtred reports status and date, second way

--- src/functions/EMSA/report.test.js
+++ src/functions/EMSA/report.test.js
@@ -29,6 +29,24 @@ describe("Reports", () => {
     }
     ]);
   });
+    it("It should filter reports by status and date ", () => {
+    expect(filter_reports2("Completed","")).toEqual([  
+    {
+        idCitizen: "10234",
+        description: "Poste inclinado con riesgo de caída.",
+        location: "Zona Norte - Avenida América",
+        date: "2026-05-10",
+        status: "Completed"
+    },
+    {
+        idCitizen: "20458",
+        description: "Cable eléctrico caído sobre la vereda.",
+        location: "Zona Sur - Calle Bolívar",
+        date: "2026-05-09",
+        status: "Completed"
+    }
+    ]);
+  });
 //   it("It should filter reports by status and date ", () => {
 //     expect(filter_reports2("Pending", "2026-05-08")).toEqual([{
 //     idCitizen: "30991",
